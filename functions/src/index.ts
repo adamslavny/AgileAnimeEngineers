1dd6015361a1178e1005ff488680939659961454
smsutherland <sagan@farthinghalearms.com>
2021-02-04 23:07:04 -0500

    firebase imports and initialization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from "firebase-functions";
+// import * as types from "@google-cloud/firestore";
+import * as admin from "firebase-admin";
+const cors = require('cors')({origin: true});
+
+const log = (name: string, something: any) => {functions.logger.info({name: name, object: something}, {structuredData: true});};
+
+admin.initializeApp();
+const db = admin.firestore()
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
