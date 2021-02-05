73b417b7761b8f6c3c48b5a1c82d21669b4c75d7
smsutherland <sagan@farthinghalearms.com>
2021-02-04 23:07:32 -0500

    rework helloWorld

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,9 @@ const db = admin.firestore()
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const helloWorld = functions.https.onRequest((request, response) => cors(request, response, () => {
+  response.set('Access-Control-Allow-Origin', '*');
+  log("body", request.body);
+  
+  response.send({data: "Hello, World!"});
+}));
