01c83c2ba9bde89edb9100569136ddbcf4690099
adamslavny <adam.slavny@ufl.edu>
2021-02-16 18:26:38 -0500

    Adding testing and updating circleCi
    Haven't added the third test because I wanted to make sure these for sure pass before creating a more complicated 3rd test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -4,6 +4,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  //Test 1
+  const linkElement = screen.getByText(/Welcome to Agile Anime Engineers/i);
   expect(linkElement).toBeInTheDocument();
+  //Test 2
+  const expected = 1;
+  const actual = 1;
+  expect(actual).toEqual(expected);
+  //Test 3
+
 });
