6a96b31e32f90421c916b504653f667e97a086bb
adamslavny <adam.slavny@ufl.edu>
2021-02-16 19:24:22 -0500

    Update App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
   //Test 1
   const linkElement = screen.getByText(/Welcome to Agile Anime Engineers/i);
@@ -14,3 +14,13 @@ test('renders learn react link', () => {
   //Test 3
 
 });
+
+test('numbers', () => {
+  render(<App />);
+  //Test 2
+  const expected = 1;
+  const actual = 1;
+  expect(actual).toEqual(expected);
+
+});
+
