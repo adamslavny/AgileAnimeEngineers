2c6d0001baa956a8c02d4979dc0f1f27d259e2d2
telesznicole@ufl.edu <5CU1NLdoogs!>
2021-02-18 14:40:20 -0500

    updates navbar, added in logo, search bar not interactive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,52 @@ import CategoryView from './CategoryView';
 import NotFound from './NotFound';
 import HomeView from './HomeView';
 import DiscussionView from './DiscussionView';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import gator from './icons/gator.svg';
+import search from './icons/search.svg';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Link to="/">
-          <h2>Welcome to Agile Anime Engineers</h2>
-        </Link>
+
+        <Navbar sticky="top" bg="info" variant="dark">
+          <Navbar.Brand href="/">
+            <img
+              alt="gator"
+              src={gator}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Animex
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link2">Link2</Nav.Link>
+            <Nav.Link href="#link3">Link3</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info"><img
+              alt="search"
+              src={search}
+              width="25"
+              height="25"
+              className="d-inline-block align-top"
+            />{' '}</Button>
+          </Form>
+        </Navbar>
+
+        <h2 className='header'>
+        <img
+              alt="gator"
+              src={gator}
+              width="75"
+              height="75"
+              className="d-inline-block align-top"
+            />{' '}</h2>
         <Switch>
           <Route exact path="/">
             <HomeView />
