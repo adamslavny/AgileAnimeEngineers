06d9a5a2cf3856eaabf1e23f22753ce6285a1681
smsutherland <sagan@farthinghalearms.com>
2021-02-19 02:39:29 -0500

    add frontend connection to addCategory

--- client/src/BackendConnection.ts
+++ client/src/BackendConnection.ts
@@ -6,16 +6,21 @@ Each of these functions returns a promise which will eventually return the resul
 Either await the functions or use .then() to access the returned data.
 */
 
-const getCategories = () => {
+export const getCategories = () => {
   console.log("calling getCategories()");
   const getCategoriesCall = functions.httpsCallable("getCategories");
   return getCategoriesCall().then((result) => {return result.data});
 };
 
-const getDiscussions = (categoryName: string) => {
+export const getDiscussions = (categoryName: string) => {
   console.log(`calling getDiscussions(${categoryName})`);
   const getDiscussionsCall = functions.httpsCallable("getDiscussions");
   return getDiscussionsCall({ category: categoryName }).then((result) => {return result.data});
 };
 
-export { getCategories, getDiscussions };
+export const addCategory = (categoryName: string) => {
+  console.log(`calling addCategory(${categoryName})`);
+  const addCategoryCall = functions.httpsCallable("addCategory");
+  return addCategoryCall({ name: categoryName }).then((result) => {return result.data});
+};
+
