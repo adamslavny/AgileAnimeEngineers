d99eb83692bac40ceec65b9cc0593fb64d08bd03
smsutherland <sagan@farthinghalearms.com>
2021-02-21 19:48:35 -0500

    frontend delete discussion button

--- client/src/views/DiscussionView.tsx
+++ client/src/views/DiscussionView.tsx
@@ -1,6 +1,20 @@
+import { useParams, useHistory } from "react-router-dom";
+import { deleteDiscussion } from "../res/BackendConnection";
+
 const DiscussionView = () => {
+  const { categoryID, discussionID } = useParams() as {categoryID: string, discussionID: string};
+
+  const history = useHistory();
+  
+  const handleDelete = () => {
+    deleteDiscussion(categoryID, discussionID).then(() => {
+      history.push(`/category/${categoryID}`);
+    });
+  }
+
   return (
     <div className="discussion-view">
+      <button onClick={handleDelete}>Delete Discussion</button>
       <p>Sorry. We don't have discussions implemented yet :3</p>
     </div>
   );
