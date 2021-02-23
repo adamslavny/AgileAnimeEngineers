8cc2c8a7c120093314401adf33f125494a45776b
adamslavny <adam.slavny@ufl.edu>
2021-02-23 14:57:24 -0500

    Temporary test fix
    Just changes title for circle ci tests. Working on unit tests but just getting circleci to work for the time being in styling

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -5,14 +5,8 @@ import App from './App';
 test('renders title', () => {
   render(<App />);
   //Test 1
-  const linkElement = screen.getByText(/Welcome to Agile Anime Engineers/i);
+  const linkElement = screen.getByText(/Animex/i);
   expect(linkElement).toBeInTheDocument();
-  //Test 2
-  const expected = 1;
-  const actual = 1;
-  expect(actual).toEqual(expected);
-  //Test 3
-
 });
 
 test('numbers', () => {
@@ -21,7 +15,6 @@ test('numbers', () => {
   const expected = 1;
   const actual = 1;
   expect(actual).toEqual(expected);
-
 });
 
 
@@ -30,5 +23,4 @@ test('rendering', () => {
   //Test 3
   let result = 5 + 2;
   expect(result).toEqual(7);
-
 });
