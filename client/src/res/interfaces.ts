c1bbe92d06cb5f4234477e429347305693e0adfd
smsutherland <sagan@farthinghalearms.com>
2021-03-18 20:23:46 -0400

    add message interface

--- client/src/res/interfaces.ts
+++ client/src/res/interfaces.ts
@@ -1,10 +1,14 @@
 export interface category {
   name: string;
   id: string;
-}
+};
 
 export interface discussion{
   name: string;
   id: string;
-}
+};
 
+export interface message{
+  content: string;
+  author: string;
+};
