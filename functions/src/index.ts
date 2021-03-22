6a1fb65b57bfc1e1d4390a3d5b5e7a8d65aacd85
smsutherland <sagan@farthinghalearms.com>
2021-03-22 00:13:49 -0400

    getting backend data returns tags as well

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,7 +23,7 @@ export const getCategories = functions.https.onRequest((request, response) => co
   log("body", request.body);
 
   const categoriesSnapshot = await db.collection("Categories").get();
-  const categoryData = categoriesSnapshot.docs.map((doc) => {return {name: doc.data().name, id: doc.id}});
+  const categoryData = categoriesSnapshot.docs.map((doc) => {return {name: doc.data().name, id: doc.id, tags: doc.data().tags}});
   response.send({data: categoryData});
 }));
 
@@ -46,8 +46,8 @@ export const getDiscussions = functions.https.onRequest((request, response) => c
   }
   const discussionsSnapshot = await categoryRef.collection("Discussions").get();
 
-  const discussionData = discussionsSnapshot.docs.map((doc) => {return {name: doc.data().name, id: doc.id}});
-  response.send({data: { name: categoryData.name, discussions: discussionData}});
+  const discussionData = discussionsSnapshot.docs.map((doc) => {return {name: doc.data().name, id: doc.id, tags: doc.data().tags}});
+  response.send({data: { name: categoryData.name, categoryTags: categoryData.tags, discussions: discussionData}});
 }));
 
 interface addCategoryRequest {
