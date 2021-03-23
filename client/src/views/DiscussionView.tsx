44dfecb70109990653bbc9f97f81fed359ac522e
smsutherland <sagan@farthinghalearms.com>
2021-03-23 17:27:25 -0400

    timestamp styling. work breaks.

--- client/src/views/DiscussionView.tsx
+++ client/src/views/DiscussionView.tsx
@@ -54,12 +54,18 @@ const DiscussionView = (props: {username: string}) => {
     setMessageText("");
   };
 
+  const pad =(num: number, size: number) => {
+    let str = num.toString();
+    while (str.length < size) str = "0" + num;
+    return str;
+}
+
   const formatDate = (date: Date) => {
     const now = Date.now();
     if(now - date.getTime() < 86400000){
-      return `${date.getHours()}:${date.getMinutes()}`;
+      return `${date.getHours()}:${pad(date.getMinutes(), 2)}`;
     }
-    return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+    return `${date.getMonth()}/${pad(date.getDate(), 2)}/${date.getFullYear()}`;
   }
 
   if(!validDiscussion){
@@ -79,9 +85,9 @@ const DiscussionView = (props: {username: string}) => {
                 {`${message.author}: ${message.content}`}
               </span>
               <span className="msg-time">
-              {formatDate(message.time)}
+                {formatDate(message.time)}
               </span>
-              </p>
+            </p>
           </div>
         );
       })}
