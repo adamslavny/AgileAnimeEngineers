e3d8ea4a378b7a191462e72e8270a5b09edfbd10
smsutherland <sagan@farthinghalearms.com>
2021-04-11 19:19:05 -0400

    set username menu

--- client/src/views/SettingsView.tsx
+++ client/src/views/SettingsView.tsx
@@ -3,10 +3,31 @@ import { userData } from "../res/interfaces";
 const SettingsView = (props: {userData: userData, updateUserData: (newUserData: userData) => void}) => {
 
   const { userData, updateUserData } = props;
+  let tempUserData = userData;
 
   return (
     <div>
-      
+      <form>
+        <label>Username:</label><br/>
+        <input
+          type="text"
+          placeholder="Please choose a username"
+          defaultValue={tempUserData.username}
+          onChange={ (event) => {
+            tempUserData.username = event.target.value;
+            console.log(tempUserData.username);
+            
+          }}
+        />
+      </form>
+      <button
+        onClick={ () => {
+          updateUserData(tempUserData);
+          
+        }}
+      >
+        Save
+      </button >
     </div>
   );
 };
