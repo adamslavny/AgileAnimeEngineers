3946ebd19f838e8764f9913d133bd00202e29ef7
smsutherland <sagan@farthinghalearms.com>
2021-04-15 01:52:20 -0400

    optimize usernameCache to not request an already requested username

--- client/src/res/UsernameCache.ts
+++ client/src/res/UsernameCache.ts
@@ -1,10 +1,13 @@
 import { getUsernames } from "./BackendConnection";
 
 let usernames: { [PUID: number]: string } = {};
+let currentRequests: { [PUID: number]: Promise<void> } = {};
 
 export const getUsernamesCache = async (PUIDs: Array<number>) => {
   let returnedUsernames: { [PUID: number]: string } = {};
   let unCachedPUIDs = Array<number>();
+
+  PUIDs = Array.from(new Set(PUIDs))
   
   PUIDs.forEach((PUID) => {
     if(usernames[PUID] !== undefined){
@@ -15,10 +18,32 @@ export const getUsernamesCache = async (PUIDs: Array<number>) => {
     }
   });
 
-  if(unCachedPUIDs.length > 0){
-    const newUsernames = (await getUsernames(unCachedPUIDs)).usernameMap;
-    usernames = {...usernames, ...newUsernames};
-    returnedUsernames = {...returnedUsernames, ...newUsernames};
-  }  
+  let promsToAwait = Array<Promise<void>>();
+  let unRequestedPUIDs = Array<number>();
+  unCachedPUIDs.forEach((PUID) => {
+    if(currentRequests[PUID] !== undefined){
+      promsToAwait.push(currentRequests[PUID].then(() => {
+        returnedUsernames[PUID] = usernames[PUID];
+      }));
+    }
+    else{
+      unRequestedPUIDs.push(PUID);
+    }
+  });
+
+  if(unRequestedPUIDs.length > 0){
+    const usernamesPromise = getUsernames(unRequestedPUIDs).then(data => data.usernameMap)
+      .then((newUsernames) => {
+        usernames = {...usernames, ...newUsernames};
+        returnedUsernames = {...returnedUsernames, ...newUsernames};
+      });
+      unRequestedPUIDs.forEach((PUID) => {
+      currentRequests[PUID] = usernamesPromise.then(() => {
+        delete currentRequests[PUID];
+      });
+    });
+    promsToAwait.push(usernamesPromise);
+  }
+  await Promise.all(promsToAwait);  
   return returnedUsernames;
 }
