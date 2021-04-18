76d0c58741ae2dce9f5493ac408a47000fa07add
smsutherland <sagan@farthinghalearms.com>
2021-04-17 22:32:06 -0400

    ListMods.tsx display a list of mod usernames

--- client/src/ListMods.tsx
+++ client/src/ListMods.tsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import { getMods } from "./res/BackendConnection";
+import { getUsernamesCache } from "./res/UsernameCache";
+
 const ListMods = () => {
+  const [modList, setModList] = useState<Array<string>>(["Loading..."]);
+
+  useEffect(() => {
+    getMods().then((newModList) => {
+      getUsernamesCache(newModList).then((modUsernamesMap) => {
+        let modUsernames: string[] = [];
+        for(let key in modUsernamesMap) {
+          modUsernames.push(modUsernamesMap[key]);
+        }
+        setModList(modUsernames);
+      });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div className="list-mods">
-      Listing mods
+      {
+        modList.map((mod, i) => {
+          return (
+            <p key={i}>
+              {mod}
+            </p>
+          );
+        })
+      }
     </div>
   );
 };
